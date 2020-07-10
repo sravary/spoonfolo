8d12010a5d84b3f85599b9bd7645b95fe6bc6f6d
sravary <sravary@hotmail.com>
2020-07-10 00:34:01 -0400

    pagination with buttons

--- src/js/index.js
+++ src/js/index.js
@@ -34,4 +34,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault(); // this keeps from reloading the page
     controlSearch();
-})
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -4,11 +4,12 @@ export default class Search {
     constructor(query) {
         this.query = query;
     }
-    async getResults(pizza) {
+    async getResults() {
         const proxy = 'https://cors-anywhere.herokuapp.com/';
         const apiKey = '0869b77be6864886ae6ab4752cc12307';
+        const num = 50;
         try {
-            const res = await axios(`${proxy}https://api.spoonacular.com/recipes/complexSearch?query=${this.query}&apiKey=${apiKey}`);
+            const res = await axios(`${proxy}https://api.spoonacular.com/recipes/complexSearch?query=${this.query}&apiKey=${apiKey}&number=${num}`);
             // console.log(res);
             this.result = res.data.results;
             // console.log(this.result);
